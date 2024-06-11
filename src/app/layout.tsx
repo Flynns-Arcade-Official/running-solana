6b190a0fb24965a35a218591599edb10b0941e38
VLong <vanlongtran2023@gmail.com>
2024-06-11 16:42:39 +0700

    Add image to open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta property="og:image" content="/Bonk.png" />
         <meta property="og:title" content="NFT Run Run" />
         <meta property="og:description" content="NFT Run Run Game" />
         <meta property="og:site_name" content="NFT Run Run" />
